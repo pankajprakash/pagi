2aca283e4d0e09e370f7481d09291799dea83614
KeyMouseIT <you@example.com>
2021-03-26 15:25:04 +0530

    pagi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
+import React,{useState,useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const[posts,setPosts]=useState([]);
+  const[loading,setLoading]=useState(false);
+  const [currentPage,setCurrentpage]=useState(1);
+  const [postsperpage,setPostsperpage]=useState();
+
+
+  useEffect(()=>{
+    const fetchPosts= async()=>{
+    setLoading(true);
+    const res=await axios.post(' http://192.168.1.10:80/api/products', {
+    "skip": 0,
+    "limit": 20,
+    "category": "",
+    "size":"",
+    "selection":"",
+    "vintage":"",
+    "min_score":"",
+    "max_score":"",
+    "min_price":"",
+    "max_price":""
+      });
+    setPosts(res.data.result)
+    setLoading(false);
+    }
+    fetchPosts();
+  },
+[]);
+  console.log(posts)
+
+
+
+ const loadmore=()=>{
+   
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{posts.map((e) => <div>{e.name}</div>)}
+
+<button onClick={loadmore}>load more</button>
+
     </div>
   );
 }
